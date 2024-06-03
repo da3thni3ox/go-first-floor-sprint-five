4a78c6c25b3941c876b36113a3646e752735f61a
Evgeniy Shatalov <darthnitrox@gmail.com>
2024-06-03 20:25:15 +0500

    Fix

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration.Hours() == 0 {
+		return 0
+	}
 	return t.distance() / t.Duration.Hours()
 }
 
@@ -172,6 +175,9 @@ type Swimming struct {
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if s.Duration.Hours() == 0 {
+		return 0
+	}
 	return s.LengthPool * float64(s.CountPool) / MInKm / s.Duration.Hours()
 }
 
